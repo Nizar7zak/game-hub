a935d0537ef0fa5c9162d397965d2603f621f0fe
macbook <macbook@macbooks-air.mynet>
2023-10-03 14:46:07 +0300

    filtering by genres

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,36 +1,44 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/apiClient";
-import { AxiosError, CanceledError } from "axios";
+import { AxiosError, AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const result = await apiClient.get<FetchResponse<T>>(endpoint, {
-          signal: controller.signal,
-        });
-        setData(result.data.results);
-        setLoading(false);
-      } catch (error) {
-        if (error instanceof CanceledError) return;
-        setError((error as AxiosError).message);
-        setLoading(false);
-      }
-    };
-    fetchData();
-    return () => controller.abort();
-  }, []);
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const result = await apiClient.get<FetchResponse<T>>(endpoint, {
+            signal: controller.signal,
+            ...requestConfig,
+          });
+          setData(result.data.results);
+          setLoading(false);
+        } catch (error) {
+          if (error instanceof CanceledError) return;
+          setError((error as AxiosError).message);
+          setLoading(false);
+        }
+      };
+      fetchData();
+      return () => controller.abort();
+    },
+    deps ? [...deps] : []
+  );
 
   return { data, error, isLoading };
 };
