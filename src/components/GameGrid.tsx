200c129ee945362aa9e9ae73df6a03d33db2a923
macbook <macbook@macbooks-MacBook-Air.local>
2023-10-03 11:42:00 +0300

    displaying genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,30 +9,24 @@ const GameGrid = () => {
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
-      {!isLoading ? (
-        <>
-          {error && <Text>{error}</Text>}
-          <SimpleGrid
-            columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-            padding="10px"
-            spacing={10}
-          >
-            {data.map((game) => (
+      {error && <Text>{error}</Text>}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={3}
+      >
+        {isLoading
+          ? skeletons.map((skeleton) => (
+              <GameCardContainer key={skeleton}>
+                <GameCardSkeleton />
+              </GameCardContainer>
+            ))
+          : data.map((game) => (
               <GameCardContainer key={game.id}>
                 <GameCard game={game} />
               </GameCardContainer>
             ))}
-          </SimpleGrid>
-        </>
-      ) : (
-        <>
-          {skeletons.map((skeleton) => (
-            <GameCardContainer key={skeleton}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        </>
-      )}
+      </SimpleGrid>
     </>
   );
 };
